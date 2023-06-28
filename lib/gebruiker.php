e18c42ca16a70ffc3341ffac01d3019efa06ad79
Aaron Saya <60886005+AaronSaya@users.noreply.github.com>
2023-06-28 09:47:25 +0200

    6. Vervaardig de User class#6

---
+++ lib/gebruiker.php
@@ -0,0 +1,22 @@
+<?php
+
+class gebruiker {
+
+     private $connection;
+
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
+
+    public function selecteerGebruiker($gebruiker_id) {
+
+        $sql = "SELECT * FROM gebruiker WHERE id = $gebruiker_id";
+
+        $result = mysqli_query($this->connection, $sql);
+        $gebruiker = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        
+        return($gebruiker);
+    }
+
+
+}

266d497ebaed3800635dfa2951ed9186e938de4d
Aaron Saya <60886005+AaronSaya@users.noreply.github.com>
2023-06-28 15:36:02 +0200

    Vervaardig de KitchenType class#7

--- lib/keukentype.php
+++ lib/keukentype.php
@@ -8,23 +8,13 @@ class keukenType {
         $this->connection = $connection;
     }
 
-    public function selecteerKeuken($keuken) {
+    public function selecteerKeukenType($keuken) {
 
-    $sql = "SELECT * FROM keuken_type WHERE id = $keuken AND record_type = 'K'";
+    $sql = "SELECT * FROM keuken_type WHERE id = $keuken";
 
     $result = mysqli_query($this->connection, $sql);
     $keuken = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
     return($keuken);
-    }
-
-    public function selecteerType($type) {
-       
-        $sql = "SELECT * FROM keuken_type WHERE id = $type ANS record_type = 'T'";
-
-        $result = mysqli_query($this->connection, $sql);
-        $type = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-        return $type;
     }
 }

9bcee9aaf7738bca08884b5b3531c3335c256c2b
Aaron Saya <60886005+AaronSaya@users.noreply.github.com>
2023-07-18 10:07:42 +0200

    8. Vervaardigen Ingredient class Fixes #9

--- lib/ingrediënten.php
+++ lib/ingrediënten.php
@@ -1,57 +1,61 @@
 <?php
 
 
-    class ingrediënt {
+class ingrediënt
+{
 
-        private $connection;
-        private $artikel;
+    private $connection;
+    private $artikel;
 
-        public function __construct($connection) {
-            $this->connection = $connection;
-            $this->artikel = new Artikel($connection);
-        }
-
-        private function getArtikel($artikel_id) {
-            return($this->artikel->selecteerArtikel($artikel_id));
-        }
-    
-        public function selecteerIngrediënt($gerecht_id) {
-
-            $sql = "select * FROM ingrediënten WHERE gerecht_id = $gerecht_id";
-            $result = mysqli_query($this->connection, $sql);
-            
-            $return = [];
-
-            while ($ingredienten = mysqli_fetch_array($result)) {
-                  
-                   
-
-                $artikel = $this->getArtikel($ingredienten['artikel_id'], MYSQLI_ASSOC);
-
-                $return [] = [
-                    "id" => $ingredienten['id'],
-                    "gerecht_id" => $ingredienten['gerecht_id'],
-                    "artikel_id" => $ingredienten['artikel_id'],
-                    "hoeveelheid" => $ingredienten['hoeveelheid'],
-                    "naam_artikel" => $artikel['naam_artikel'],
-                    "omschrijving" => $artikel["omschrijving"],
-                    "prijs" => $artikel["prijs"],
-                    "eenheid" => $artikel["eenheid"],
-                    "hoeveelheid_verpakking" => $artikel["hoeveelheid_verpakking"],
-                    "calorie" => $artikel["calorie"],
-
-                ];
-               
-
-               
-            }
-
-        
-                return $return;
-        }
+    public function __construct($connection)
+    {
+        $this->connection = $connection;
+        $this->artikel = new Artikel($connection);
     }
-        
-    
+
+    private function getArtikel($artikel_id)
+    {
+        return ($this->artikel->selecteerArtikel($artikel_id));
+    }
+
+    public function selecteerIngrediënt($gerecht_id)
+    {
+
+        $sql = "select * FROM ingrediënten WHERE gerecht_id = $gerecht_id";
+        $result = mysqli_query($this->connection, $sql);
+
+        $return = [];
+
+        while ($ingredienten = mysqli_fetch_array($result)) {
+
+
+
+            $artikel = $this->getArtikel($ingredienten['artikel_id'], MYSQLI_ASSOC);
+
+            $return[] = [
+                "id" => $ingredienten['id'],
+                "gerecht_id" => $ingredienten['gerecht_id'],
+                "artikel_id" => $ingredienten['artikel_id'],
+                "hoeveelheid" => $ingredienten['hoeveelheid'],
+                "naam_artikel" => $artikel['naam_artikel'],
+                "omschrijving" => $artikel["omschrijving"],
+                "prijs" => $artikel["prijs"],
+                "eenheid" => $artikel["eenheid"],
+                "hoeveelheid_verpakking" => $artikel["hoeveelheid_verpakking"],
+                "calorie" => $artikel["calorie"],
+
+            ];
+
+
+
+        }
+
+
+        return $return;
+    }
+}
+
+
 
 
 ?>
